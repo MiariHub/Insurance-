7acac3447ec8c97903ef8c69a714da56974e564e
Mohamad Meaari <123135462+MiariHub@users.noreply.github.com>
2024-05-14 03:11:35 +0300

    project.py

--- project.py
+++ project.py
@@ -180,7 +180,7 @@ def main():
    
 
     # Use the HTML component to add a fixed footer
-    st.markdown('<div style="{}">Prdiction Issurance using AI.</div>'.format(footer_style), unsafe_allow_html=True)
+    st.markdown('<div style="{}">This app is continuously being improved and updated following Agile practices.</div>'.format(footer_style), unsafe_allow_html=True)
 
 if __name__ == "__main__":
     main()
