e7b171adc03bfbbe7dc040ce53404063664b0592
Mohamad Meaari <123135462+MiariHub@users.noreply.github.com>
2024-05-14 03:02:21 +0300

    project.py

--- project.py
+++ project.py
@@ -148,7 +148,7 @@ with tab3:
             st.success(f'Predicted Insurance Cost: ${prediction[0]:.2f}')
     
 with tab4:
-    st.write("""
+     st.write("""
     ### About the Application
     This application uses a machine learning model to predict individual healthcare costs based on personal health data. The goal is to help users explore how different factors might influence their medical insurance premiums.
     #### How to Use
